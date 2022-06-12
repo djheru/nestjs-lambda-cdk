bf74596dc438688fe20a260c85ca9d0d1847a25e
Philip Damra <pdamra@dealerinspire.com>
2022-06-12 12:06:59 -0700

    lol:" " ; ; ;

--- bin/nestjs-lambda-cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { NestjsLambdaCdkStack } from '../lib/nestjs-lambda-cdk-stack';
-
-const app = new cdk.App();
-new NestjsLambdaCdkStack(app, 'nestjs-lambda-cdk', {});
